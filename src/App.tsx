7bd42f9ea61716d10d683e86adec0690ecc3420c
Sebastien De Varennes <sebdevar@gmail.com>
2023-03-06 19:43:04 +0100

    Create bowling game grid

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,74 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
+            <header className="header">
                 Bowling Game Kata
             </header>
+            <div className="score-sheet">
+                <div className="player-row" >
+                    <div className="frame">
+                        1
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="score"/>
+                    </div>
+                    <div className="frame">
+                        2
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="score"/>
+                    </div>
+                    <div className="frame">
+                        3
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="score"/>
+                    </div>
+                    <div className="frame">
+                        4
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="score"/>
+                    </div>
+                    <div className="frame">
+                        5
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="score"/>
+                    </div>
+                    <div className="frame">
+                        6
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="score"/>
+                    </div>
+                    <div className="frame">
+                        7
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="score"/>
+                    </div>
+                    <div className="frame">
+                        8
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="score"/>
+                    </div>
+                    <div className="frame">
+                        9
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="score"/>
+                    </div>
+                    <div className="frame last-frame">
+                        10
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="roll"/>
+                        <div className="score"/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
